607c7da7eff292555e6a85a0ef1a245df6264246
felipe <felipeurrego29@gmail.com>
2023-09-22 11:35:14 -0500

    fin proyecto

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,38 +1,58 @@
 const { response, request } = require('express');
+const bcryptjs = require('bcryptjs'); 
+const Usuario = require('../models/usuario');
 
-const usuariosGet = (req = request, res = response) => {
+const usuariosGet = async(req = request, res = response) => {
 
-    const { q, nombre = 'No name', apikey, page = 1, limit } = req.query 
+    const { limite = 5, desde = 0 } = req.query;
+    const query = { estado: true};
+
+     const [total, usuarios] = await Promise.all([
+        Usuario.countDocuments({query}),
+        Usuario.find({query})
+           .skip(Number(desde))
+           .limit(Number (limite))
+    ]);
 
     res.json({
-        msg: 'get API - controlador',
-        q,
-        nombre,
-        apikey,
-        page,
-        limit
+        total,
+        usuarios
     });
 }
 
-const usuariosPost = (req, res = response) => {
-      
-    const {nombre, edad} = req.body;
+const usuariosPost = async(req, res = response) => {
+                 
+    const {nombre, correo, password, rol} = req.body;
+    const usuario = new Usuario({nombre, correo, password, rol});
 
+    //Encriptar la contraseña
+    const salt = bcryptjs.genSaltSync();
+    usuario.password = bcryptjs.hashSync( password, salt);
+       
+    //Guardar en BD
+    await usuario.save();
+ 
     res.json({
         msg: 'post API - usuariosPost',
-        nombre,
-        edad 
+        usuario
     });
 }
 
-const usuariosPut = (req, res = response) => {
+const usuariosPut = async(req, res = response) => {
 
     const { id } = req.params;
+    const { _id, password, google, correo, ...resto } = req.body;
 
-    res.json({
-        msg: 'put API - usuarioPut',
-        id
-    });
+    //TODO validar contra la base de datos
+    if(password) {
+        const salt = bcryptjs.genSaltSync();
+        resto.password = bcryptjs.hashSync( password, salt);
+    }
+
+    const usuario = await Usuario.findByIdAndUpdate(id, resto);
+
+
+    res.json(usuario);
 } 
 
 const usuariosPatch = (req, res = response) => {
@@ -41,10 +61,15 @@ const usuariosPatch = (req, res = response) => {
     });
 }
 
-const usuariosDelete = (req, res = response) => {
-    res.json({
-        msg: 'delete API - usuarioDelete'
-    });
+const usuariosDelete = async(req, res = response) => {
+
+    const {id} = req.params;
+
+    // Fiscamente lo borramos
+    // const usuario = await Usuario.findByIdAndDelete(id);
+    const usuario = await Usuario.findByIdAndUpdate(id, {estado: false});
+
+    res.json(usuario);
 }
 
 module.exports = {
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,4 +1,7 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos');
+const { esRoleValido, emailExiste, existeUsuarioPorId } = require('../helpers/db-validators');
 const { usuariosGet,
         usuariosPost,
         usuariosPut,
@@ -9,12 +12,29 @@ const router = Router();
 
 router.get('/', usuariosGet);
 
-router.post('/', usuariosPost);
+router.post('/',[
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('password', 'El password debe de ser de mas de 6 letras').isLength({min:6}),
+    check('correo', 'El correo no es valido').isEmail(),
+    check('correo').custom(emailExiste),
+    // check('rol', 'No es un rol valido').isIn(['ADMIN_ROLE','USER_ROLE']),
+    check('rol').custom( esRoleValido),
+    validarCampos
+], usuariosPost);
 
-router.put('/:id', usuariosPut);
+router.put('/:id',[
+    check('id', 'No es un ID valido').isMongoId(),
+    check('id').custom( existeUsuarioPorId ),
+    check('rol').custom(esRoleValido),  
+    validarCampos
+], usuariosPut);
 
 router.patch('/', usuariosPatch);
 
-router.delete('/', usuariosDelete);
+router.delete('/:id',[
+    check('id', 'No es un ID valido').isMongoId(),
+    check('id').custom( existeUsuarioPorId ),
+    validarCampos
+], usuariosDelete);
 
 module.exports = router;
